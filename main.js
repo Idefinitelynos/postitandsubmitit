5d21e279067c67a05d780f2147ffdbbc48a3c47d
Idefinitelynos <95195388+Idefinitelynos@users.noreply.github.com>
2022-08-24 20:09:44 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,30 +1,29 @@
+noseX=0;
+noseY=0;
 function setup() {
-    canvas=createCanvas(500, 500)
+    canvas=createCanvas(300,300)
     canvas.center()
     video=createCapture(VIDEO)
     video.hide()
+    poseNet=ml5.poseNet(video, modelloaded)
+    poseNet.on("pose", gotposes)
 }
 function draw() {
-    image(video, 100, 100, 300, 300)
-    circle(250, 50, 20 )
-    rect(125, 35, 40, 40)
-    ellipse(56, 46, 55, 76)
-    rect(30, 250, 55, 55, 20);
-    rect(15, 125, 60, 90)
-    circle(35, 375, 50 )
-    triangle(70, 380, 45, 450, 105, 450);
-    rect(10, 452, 200, 45,);
-    circle(125, 425, 35 )
-    rect(225, 410, 80, 80)
-    ellipse(370, 450, 100, 50)
-    circle(450, 400,80 )
-    rect(420, 280, 70, 70, 15);
-    triangle(485, 200, 415, 270, 485, 270);
-    circle(430, 215, 35 )
-    rect(418, 80, 65, 100)
-    ellipse(418, 46, 76, 55)
-    rect(320, 35, 40, 40)
+    image(video, 0, 0, 300, 300)
+    image(mustache, noseX-40, noseY-60, 50, 50)
 }
-function oe() {
-    save("photo.png")
+function modelloaded() {
+    console.log("poseNet is prepared")
+}
+function gotposes( results) {
+    if (results.length>0) {
+        console.log(results)
+        console.log("noseX="+results[0].pose.nose.x)
+        console.log("noseY="+results[0].pose.nose.y)
+        noseX=results[0].pose.nose.x
+        noseY=results[0].pose.nose.y
+    }
+}
+function preload() {
+    mustache=loadImage("https://i.postimg.cc/3x3QzSGq/m.png")
 }
